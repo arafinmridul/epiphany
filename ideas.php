47f8a0f77ca0816379b9ed202a223c912638a825
Arafin Mridul <mridularafin@gmail.com>
2024-03-15 05:40:59 +0600

    ideas show up

--- ideas.php
+++ ideas.php
@@ -1,24 +1,28 @@
 <?php include 'inc/header.php'; ?>
 
-<h2>Ideas</h2>
+<?php
+$sql = 'SELECT * FROM ideas';
+$result = mysqli_query($conn, $sql);
+$ideas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
 
-<div class="card my-3">
-    <div class="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+<h2> Past Ideas </h2>
+<?php if (empty($ideas)) : ?>
+    <p class="lead mt-3">
+        There is no idea.
+    </p>
+<?php endif; ?>
+
+<?php foreach ($ideas as $idea) : ?>
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <?php echo $idea['body']; ?>
+            <div class="text-secondary mt-2">
+                By <?php echo $idea['name']; ?>
+                On <?php echo $idea['date']; ?>
+            </div>
+        </div>
     </div>
-</div>
-
-<div class="card my-3">
-    <div class="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-    </div>
-</div>
-
-<div class="card my-3">
-    <div class="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-    </div>
-</div>
-
+<?php endforeach; ?>
 
 <?php include 'inc/footer.php'; ?>

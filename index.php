956a1828f5bfdb937581ac2c7fb91f2787064e37
Arafin Mridul <mridularafin@gmail.com>
2024-03-15 06:38:50 +0600

    err check

--- index.php
+++ index.php
@@ -36,15 +36,24 @@ if (isset($_POST['submit'])) {
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="mt-4 w-75">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        <input type="text" class="form-control <?php echo !$nameErr ?: 'is-invalid' ?>" id="name" name="name" placeholder="Enter your name">
+        <div class="invalid-feedback">
+            <?php echo $nameErr; ?>
+        </div>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+        <input type="email" class="form-control <?php echo !$emailErr ?: 'is-invalid' ?>" id="email" name="email" placeholder="Enter your email">
+        <div class="invalid-feedback">
+            <?php echo $emailErr; ?>
+        </div>
     </div>
     <div class="mb-3">
         <label for="body" class="form-label">Idea</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Share your idea"></textarea>
+        <textarea class="form-control  <?php echo !$bodyErr ?: 'is-invalid' ?>" id="body" name="body" placeholder="Share your idea"></textarea>
+        <div class="invalid-feedback">
+            <?php echo $bodyErr; ?>
+        </div>
     </div>
     <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
